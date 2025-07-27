ec990ef79ad7e00ac25d90e1101f4a9ba136ff88
paines <anes.lihovac@gmail.com>
2025-07-27 12:39:23 +0200

    comanche style

--- game.fnl
+++ game.fnl
@@ -1,32 +1,55 @@
 (local width 400)
 (local height 300)
-(local terrain
-  (let [arr {}]
-    (for [i 1 width]
-      (tset arr i (+ 100 (* 50 (math.sin (/ i 20))))))
-    arr))
-(local heli {:x 50 :y 50 :vy 0})
+(local terrain-state {:width 256 :height 256 :data nil})
 
-(fn update [dt]
-  (when (love.keyboard.isDown "right") (tset heli :x (+ heli.x (* 100 dt))))
-  (when (love.keyboard.isDown "left") (tset heli :x (- heli.x (* 100 dt))))
-  (when (love.keyboard.isDown "up") (tset heli :vy (- heli.vy (* 200 dt))))
-  (when (love.keyboard.isDown "down") (tset heli :vy (+ heli.vy (* 200 dt))))
-  (tset heli :y (+ heli.y (* heli.vy dt)))
-  (tset heli :vy (* heli.vy 0.98))) ; Dämpfung
+(local cam {:x 128 :y 64 :z 40 :angle 0})
 
-(fn draw-world []
-  (love.graphics.setColor 0.3 0.8 0.3)
-  (for [i 1 width]
-    (love.graphics.line i height i (- height (. terrain i)))))
+(fn load-heightmap [filename]
+  (let [img (love.image.newImageData filename)
+        imgw (img:getWidth)
+        imgh (img:getHeight)
+        arr {}]
+    (for [x 0 (- imgw 1)]
+      (tset arr x {})
+      (for [y 0 (- imgh 1)]
+        (let [r (select 1 (img:getPixel x y))]
+          (tset (. arr x) y (* r 255))))) ; Höhe von 0-255
+    {:data arr :width imgw :height imgh}))
+
+(fn draw-terrain []
+  (let [terrain terrain-state.data
+        terrain-width terrain-state.width
+        terrain-height terrain-state.height]
+    (for [screen-x 0 (- width 1)]
+      (let [angle (+ cam.angle (* (- screen-x (/ width 2)) 0.005))]
+        (local max-screen-y height)
+        (for [depth 1 200]
+          (let [dx (* (math.cos angle) depth)
+                dy (* (math.sin angle) depth)
+                map-x (math.floor (+ cam.x dx))
+                map-y (math.floor (+ cam.y dy))]
+            (when (and (>= map-x 0) (< map-x terrain-width)
+                       (>= map-y 0) (< map-y terrain-height))
+              (let [h (. (. terrain map-x) map-y)
+                    screen-y (math.floor (- cam.z (* 0.5 h) (/ 100 depth)))]
+                (when (< screen-y max-screen-y)
+                  (love.graphics.setColor 0.2 0.8 0.2 (* 1.0 (- 1 (/ depth 200))))
+                  (love.graphics.line screen-x screen-y screen-x max-screen-y)
+                  (set! max-screen-y screen-y))))))))))
 
 (fn draw []
-  (draw-world)
-  (love.graphics.setColor 1 1 0)
-  (love.graphics.rectangle "fill" heli.x (- height heli.y) 20 10))
+  (draw-terrain))
 
 (fn load []
+  (let [tstate (load-heightmap "heightmap.png")]
+    (tset terrain-state :data tstate.data)
+    (tset terrain-state :width tstate.width)
+    (tset terrain-state :height tstate.height))
   (love.window.setMode width height)
-  (love.window.setTitle "Helikopter über Terrain"))
+  (love.window.setTitle "Voxel Terrain Demo"))
+
+(fn update [dt]
+  ;; Helikopter/Kamera-Steuerung (z.B. mit Tasten)
+  )
 
 {:update update :draw draw :load load}
--- game.lua
+++ game.lua
@@ -1,49 +1,61 @@
 local width = 400
 local height = 300
-local terrain
-do
+local terrain_state = {width = 256, height = 256, data = nil}
+local cam = {x = 128, y = 64, z = 40, angle = 0}
+local function load_heightmap(filename)
+  local img = love.image.newImageData(filename)
+  local imgw = img:getWidth()
+  local imgh = img:getHeight()
   local arr = {}
-  for i = 1, width do
-    arr[i] = (100 + (50 * math.sin((i / 20))))
+  for x = 0, (imgw - 1) do
+    arr[x] = {}
+    for y = 0, (imgh - 1) do
+      local r = select(1, img:getPixel(x, y))
+      do end (arr[x])[y] = (r * 255)
+    end
   end
-  terrain = arr
+  return {data = arr, width = imgw, height = imgh}
 end
-local heli = {x = 50, y = 50, vy = 0}
-local function update(dt)
-  if love.keyboard.isDown("right") then
-    heli["x"] = (heli.x + (100 * dt))
-  else
-  end
-  if love.keyboard.isDown("left") then
-    heli["x"] = (heli.x - (100 * dt))
-  else
-  end
-  if love.keyboard.isDown("up") then
-    heli["vy"] = (heli.vy - (200 * dt))
-  else
-  end
-  if love.keyboard.isDown("down") then
-    heli["vy"] = (heli.vy + (200 * dt))
-  else
-  end
-  heli["y"] = (heli.y + (heli.vy * dt))
-  do end (heli)["vy"] = (heli.vy * 0.98)
-  return nil
-end
-local function draw_world()
-  love.graphics.setColor(0.3, 0.8, 0.3)
-  for i = 1, width do
-    love.graphics.line(i, height, i, (height - terrain[i]))
+local function draw_terrain()
+  local terrain = terrain_state.data
+  local terrain_width = terrain_state.width
+  local terrain_height = terrain_state.height
+  for screen_x = 0, (width - 1) do
+    local angle = (cam.angle + ((screen_x - (width / 2)) * 0.005))
+    local max_screen_y = height
+    for depth = 1, 200 do
+      local dx = (math.cos(angle) * depth)
+      local dy = (math.sin(angle) * depth)
+      local map_x = math.floor((cam.x + dx))
+      local map_y = math.floor((cam.y + dy))
+      if ((map_x >= 0) and (map_x < terrain_width) and (map_y >= 0) and (map_y < terrain_height)) then
+        local h = terrain[map_x][map_y]
+        local screen_y = math.floor((cam.z - (0.5 * h) - (100 / depth)))
+        if (screen_y < max_screen_y) then
+          love.graphics.setColor(0.2, 0.8, 0.2, (1.0 * (1 - (depth / 200))))
+          love.graphics.line(screen_x, screen_y, screen_x, max_screen_y)
+          __fnl_global__set_21(max_screen_y, screen_y)
+        else
+        end
+      else
+      end
+    end
   end
   return nil
 end
 local function draw()
-  draw_world()
-  love.graphics.setColor(1, 1, 0)
-  return love.graphics.rectangle("fill", heli.x, (height - heli.y), 20, 10)
+  return draw_terrain()
 end
 local function load()
+  do
+    local tstate = load_heightmap("heightmap.png")
+    do end (terrain_state)["data"] = tstate.data
+    terrain_state["width"] = tstate.width
+    terrain_state["height"] = tstate.height
+  end
   love.window.setMode(width, height)
-  return love.window.setTitle("Helikopter \195\188ber Terrain")
+  return love.window.setTitle("Voxel Terrain Demo")
+end
+local function update(dt)
 end
 return {update = update, draw = draw, load = load}
